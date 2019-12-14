a8229b0ac1a0075f6de6ff3bf5531abbbeb22f2a
Aaron Kwek <201601132@iacademy.edu.ph>
2019-12-14 21:37:43 +0800

    Change queries to be specific
    ForgotPassword changes

--- java/invaid/users/action/ForgotPasswordAction.java
+++ java/invaid/users/action/ForgotPasswordAction.java
@@ -20,39 +20,60 @@ public class ForgotPasswordAction extends ActionSupport implements ModelDriven{
 	//Execute
 	public String execute() {
 		if(checkRecords()) {
-			Mail.sendPasswordResetMail(user);
+			System.out.println("Account exist");
+			//Generate token
+			//Needed from Raph's Part
+			
+			//Update account status to forgot password
+			//Neede from Raph's Part
+			
+			//Send email
+			//Mail.sendPasswordResetMail(user);
+			
 			return SUCCESS;
 		}
+		System.out.println("Account does not exist");
 		return ERROR;
 	}
 
 	//Functions
+	/* This is for object retrieval
+	 * public boolean checkRecords() { boolean recordsExist = false; List
+	 * recordsList = getRecords();
+	 * 
+	 * if(recordsList != null) { //System.out.println("No. of Records:" +
+	 * recordsList.size()); UserAccountBean temp; for(int i = 0; i <
+	 * recordsList.size(); i++) { temp = (UserAccountBean) recordsList.get(i);
+	 * if(user.getUser_email().equals(temp.getUser_email())) {
+	 * //System.out.println("Record Exist"); //Token should be set here from temp to
+	 * user //temp.setUser_accountId(user.getUser_accountId()); //Update account
+	 * status //temp.setUser_accountStatus(1,0,-1); recordsExist = true; } } }
+	 * return recordsExist; }
+	 */
+	
 	public boolean checkRecords() {
 		boolean recordsExist = false;
 		List recordsList = getRecords();
 		
-		if(recordsList != null) {
-			System.out.println("No. of Records:" + recordsList.size());
-			UserAccountBean temp;
-			for(int i = 0; i < recordsList.size(); i++) {
-				temp = (UserAccountBean) recordsList.get(i);
-				if(user.getUser_email().equals(temp.getUser_email())) {
-					System.out.println("Records Exist");
-					//Token should be set here from temp to user
-					//temp.setUser_accountId(user.getUser_accountId());
+		if(recordsList!=null) {
+			String resultEmail;
+			
+			for(int i=0; i< recordsList.size(); i++) {
+				resultEmail = (String) recordsList.get(i);
+				if(resultEmail.equals(user.getUser_email())) {
 					recordsExist = true;
 				}
 			}
 		}
 		return recordsExist;
 	}
+	
 	private List getRecords() {
 		List recordsList = new ArrayList();
 		
 		try {
 			session.beginTransaction();
-			
-			recordsList = session.createQuery("FROM UserAccountBean").list();
+			recordsList = session.createQuery("SELECT account.user_email from UserAccountBean as account").list();
 		}
 		catch(Exception sqle) {
 			if(null != session.getTransaction()) {
@@ -68,7 +89,7 @@ public class ForgotPasswordAction extends ActionSupport implements ModelDriven{
 		}
 		return recordsList;
 	}
-	
+
 	//Getters and Setters
 	public UserAccountBean getUser() {
 		return user;
