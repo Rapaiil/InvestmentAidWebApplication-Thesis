c81cdfda552afce22ace31b117aefd6b641dd5b9
Aaron Kwek <201601132@iacademy.edu.ph>
2020-02-14 14:17:43 +0800

    Fixed Client Side Validation
    Fixed frontend
    Fixed backend

--- web/js/validations.js
+++ web/js/validations.js
@@ -1,54 +1,78 @@
 $(function(){
 	//Email
 	$("#ea").change(function(){
-		//alert("Triggered #ea on change");
+		alert("Triggered #ea on change");
 		var email 			= $.trim($("#ea").val());
 		var isEmailValid 	= ValidateEmail.validate(email);
 		var errmsg			= $("#lea");
 		
-		if(isEmailValid){
+		if(email == ""){
+			errmsg.html("This field is required");
+			errmsg.css("color", "red");
+			errmsg.css("visibility","visible");
+		}
+		else if(isEmailValid){
 			errmsg.html("");
 			errmsg.css("color", "black");
-			errmsg.css("display","none");
+			errmsg.css("visibility","hidden");
 		}
 		else{
 			errmsg.html("Please enter a valid email");
 			errmsg.css("color", "red");
-			errmsg.css("display","block");
+			errmsg.css("visibility","visible");
 		}
 	});
 	
 	//Password
 	$("#p,#cp").change(function(){
-		//alert("Triggered #p and #cp on change");
+		alert("Triggered #p and #cp on change");
 		var password 			= $.trim($("#p").val());
 		var confirmPassword		= $.trim($("#cp").val());
 		var isPasswordValid 	= ValidatePassword.validate(password);
 		var errmsg				= $("#lp");
+		var errmsg1				= $("#lcp");
 		
-		if(isPasswordValid){
+		if(password == ""){
+			errmsg.html("This field is required");
+			errmsg.css("color", "red");
+			errmsg.css("visibility","visible");
+		}
+		else if(confirmPassword == ""){
+			errmsg1.html("This field is required");
+			errmsg1.css("color", "red");
+			errmsg1.css("visibility","visible");
+		}
+		else if(isPasswordValid){
 			var isPasswordMatch = ValidatePassword.match(password,confirmPassword);
 			if(isPasswordMatch){
 				errmsg.html("Passwords Match");
 				errmsg.css("color", "green");
-				errmsg.css("display","block");
+				errmsg.css("visibility","visible");
+				errmsg1.html("");
+				errmsg1.css("color", "green");
+				errmsg1.css("visibility","hidden");
 			}
 			else{
 				errmsg.html("Passwords doesn't match");
 				errmsg.css("color", "red");
-				errmsg.css("display","block");
+				errmsg.css("visibility","visible");
+				errmsg1.html("");
+				errmsg1.css("color", "green");
+				errmsg1.css("visibility","hidden");
 			}
 			//errmsg.html("");
 			//errmsg.css("color", "black");
 			//errmsg.css("display","none");
 		}
 		else{
-			errmsg.html("Please make sure your password contains a mix of at least 1 uppercase, 1 lowercase and 1 numerical characters");
+			errmsg.html("Passowrd must contain at least 1 uppercase, 1 lowercase and 1 numerical characters");
 			errmsg.css("color", "red");
-			errmsg.css("display","block");
+			errmsg.css("visibility","visible");
+			errmsg1.html("");
+			errmsg1.css("color", "green");
+			errmsg1.css("visibility","hidden");
 		}
 	});
-	
 });
 
 
