53f290295b8415badac58283c8b1966895afae01
Raphael Feliciano <raphael_feliciano@ymail.com>
2020-03-04 05:36:19 +0800

    added xml for uitf

---
+++ java/invaid/users/model/BankNames.java
@@ -0,0 +1,29 @@
+package invaid.users.model;
+
+public interface BankNames {
+	final String BANK_1 = "Metropolitan Bank & Trust Co.";
+	final String BANK_2 = "Philippine National Bank";
+	final String BANK_3 = "BPI Asset Management and Trust Corporation";
+	final String BANK_4 = "Asia United Bank";
+	final String BANK_5 = "Bank of Commerce";
+	final String BANK_6 = "BDO Unibank, Inc.";
+	final String BANK_7 = "China Banking Corporation";
+	final String BANK_8 = "EastWest Banking Corporation";
+	final String BANK_9 = "LandBank of the Philippines";
+	final String BANK_10 = "Philippine Bank of Communications";
+	final String BANK_11 = "Rizal Commercial Banking Corporation";
+	final String BANK_12 = "Security Bank Corporation";
+	final String BANK_13 = "Union Bank";
+	final String BANK_14 = "United Coconut Planters Bank";
+	final String BANK_15 = "Development Bank of the Philippines";
+	final String BANK_16 = "AB Capital";
+	final String BANK_17 = "Philippine Business Bank";
+	final String BANK_18 = "ATRAM Trust Corporation";
+	final String BANK_19 = "Philippine Savings Bank";
+	final String BANK_20 = "Sterling Bank of Asia";
+	final String BANK_21 = "Robinsons Bank";
+	final String BANK_22 = "CTBC Bank (Philippines) Corp.";
+	final String BANK_23 = "Manulife Asset Management and Trust Corporation (MAMTC)";
+	final String BANK_24 = "Maybank Philippines Inc.";
+	final String BANK_25 = "Pru Life UK Investments";
+}
